9c1ade1595d5b2fd675cb8addb5b5a171dbacc31
mikez <zeldabakhos6@gmail.com>
2025-03-14 11:30:33 +0100

    arithmetic operation

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
-const express = require("express")
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+
+// MIDDLEWARE
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    req.arithmetical_value = 4 * 7; 
+    next();
+});
 
 app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+    res.send(`Request Time: ${req.requestTime} | Arithmetical Value: ${req.arithmetical_value}`);
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log(`Example app listening at http://localhost:${port}`);
+});
