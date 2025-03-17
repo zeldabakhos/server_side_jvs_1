5e1aa376a4942a75dfdfd43a6b5180d73611d69b
mikez <zeldabakhos6@gmail.com>
2025-03-17 10:40:16 +0100

    cors

--- src/index.js
+++ src/index.js
@@ -14,6 +14,16 @@ app.use(express.json())
 // ROUTES
 app.use("/api/users", userRoutes)
 
+// cors middleware
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header(
+     "Access-Control-Allow-Headers",
+     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    )
+    next()
+   })
+
 app.get("/", (req, res) => {
     res.send("Welcome to my API ! e-commerce backed 🤳")
    })
