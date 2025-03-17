f8c4aef28ede7d09fdf7a9fca9591ffc4b91d849
mikez <zeldabakhos6@gmail.com>
2025-03-17 10:25:04 +0100

    final changes to GET/POST

--- src/routes/users.js
+++ src/routes/users.js
@@ -6,7 +6,14 @@ router.get('/', (req,res) => {
 })
 
 router.post('/', (req,res) => {
-    res.send("you have reached the post request!")
+    // Get the data from the request
+    const { firstName, email, password } = req.body
+    res.json({
+        firstName,
+        email,
+        password,
+        _id: "randomId4567",
+    })
 })
 
 
