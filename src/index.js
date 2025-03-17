dbdca389227f04273c842b8a0e22a1acc0720e9c
mikez <zeldabakhos6@gmail.com>
2025-03-17 10:18:33 +0100

    adding GET and POST actions

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const userRoutes = require("./routes/users")
 
 // MIDDLEWARE
-app.use((req, res, next) => {
-    req.requestTime = Date.now();
-    req.arithmetical_value = 4 * 7; 
-    next();
-});
+app.use(express.json())
+// app.use((req, res, next) => {
+//     req.requestTime = Date.now();
+//     req.arithmetical_value = 4 * 7; 
+//     next();
+// });
+
+// ROUTES
+app.use("/api/users", userRoutes)
 
 app.get("/", (req, res) => {
-    res.send(`Request Time: ${req.requestTime} | Arithmetical Value: ${req.arithmetical_value}`);
-});
+    res.send("Welcome to my API ! e-commerce backed 🤳")
+   })
+
+// app.get("/", (req, res) => {
+//     res.send(`Request Time: ${req.requestTime} | Arithmetical Value: ${req.arithmetical_value}`);
+// });
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
