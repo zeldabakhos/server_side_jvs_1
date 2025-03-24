b83a1b311d21174d332d4dde8eaec539f709496d
mikez <zeldabakhos6@gmail.com>
2025-03-24 09:12:23 +0100

    change of  auth

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,14 +1,20 @@
 const jwt = require("jsonwebtoken")
 
-exports.verifyToken = (req, res, next) =>{
+exports.verifyToken = async (req, res, next) =>{
     if (!req.headers.authorization) {
         return res.status(403).send({ message: "No token provided!" });
     }
 
-    const token = req.headers.authorization.split(" ")[1];
     try {
+        const token = req.headers.authorization.split(" ")[1];
         const decodedToken = jwt.verify(token, process.env.SECRET_TOKEN_KEY);
         req.userId = decodedToken.userId;
+
+        const user= await User.findById(req.userId)
+
+        if (!user){
+            return res.status(404).send({ message: "User not found!" });
+        }
         next();
     } catch (err) {
         return res.status(401).send({ message: "Unauthorized!" });
